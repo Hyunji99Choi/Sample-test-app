4337d688948d5a5744195c9653354e2121d61d13
최현지 <tyg06229@gmail.com>
2025-05-23 19:36:05 +0900

    feat: web 검색 전체 페이지 UI 연결

--- app/src/main/java/com/example/sampleapp/presentation/ui/website/WebsiteFragment.kt
+++ app/src/main/java/com/example/sampleapp/presentation/ui/website/WebsiteFragment.kt
@@ -2,12 +2,36 @@ package com.example.sampleapp.presentation.ui.website
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.sampleapp.databinding.FragmentWebsiteBinding
 import com.example.sampleapp.presentation.ui.BaseFragment
+import com.example.sampleapp.presentation.util.repeatOnStarted
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
 @AndroidEntryPoint
 class WebsiteFragment : BaseFragment<FragmentWebsiteBinding>() {
+
+    private val viewModel: WebsiteViewModel by viewModels()
+
+    override fun setUI() {
+        super.setUI()
+
+        binding.composeView.setContent {
+            WebSiteScreen(viewModel)
+        }
+    }
+
+    override fun setObserver() {
+        super.setObserver()
+
+        repeatOnStarted(viewLifecycleOwner) {
+            viewModel.error.collectLatest { error ->
+                displaySnackBar(error.second)
+            }
+        }
+    }
+
 
 
     override fun getViewBinding(
