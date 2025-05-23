126b771ed9104aa2c26297105e229f1209ccf2df
최현지 <tyg06229@gmail.com>
2025-05-23 19:25:44 +0900

    test: itemData fake 데이터

--- app/src/main/java/com/example/sampleapp/domain/model/ItemData.kt
+++ app/src/main/java/com/example/sampleapp/domain/model/ItemData.kt
@@ -36,3 +36,35 @@ data class WebItemData(
     override val url: String,
     override val dateTime: Date,
 ) : ItemData()
+
+
+fun getFakeBlog(): BlogItemData {
+    return BlogItemData(
+        title = "블로그 제목입니다",
+        contents = "블로그 내용이 여기에 들어갑니다.",
+        url = "https://example.com",
+        dateTime = Date(),
+        name = "블로그 작성자",
+        thumbnail = "https://example.com/thumbnail.jpg"
+    )
+}
+
+fun getFakeCafe(): CafeItemData {
+    return CafeItemData(
+        title = "카페 리뷰입니다.",
+        contents = "맛있는 커피와 좋은 분위기에 대해 이야기합니다.",
+        url = "https://example.com/cafe",
+        dateTime = Date(),
+        name = "카페 이름",
+        thumbnail = "https://example.com/cafe_thumbnail.jpg"
+    )
+}
+
+fun getFakeWeb(): WebItemData {
+    return WebItemData(
+        title = "구글",
+        url = "https://www.google.com",
+        dateTime = Date(),
+        contents = "구글은 세계 최대의 검색 엔진입니다."
+    )
+}
